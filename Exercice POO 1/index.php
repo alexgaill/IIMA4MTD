8f1d1f06edd2c10c07808e3d35a2560bb0b0680f
alexandre gaillot <alexandre@MacBook-Air-de-alexandre.local>
2020-12-09 11:45:59 +0100

    ajout consigne exercice POO part1

---
+++ Exercice POO 1/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Vous allez travailler sur la mise en place d'un blog en POO
+// Vous allez créer dans un premier temps la class Database qui va gérer la connexion avec la base de donnée.
+// Vous créez une class qui gère les catégories (récupération des catégories et enregistrement d'une nouvelle catégorie)
+// Vous mettrez en place une page d'accueil qui affiche une liste des catégories existantes
